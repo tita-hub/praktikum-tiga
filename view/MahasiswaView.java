e444de05bd71968c7a6fe5354e52b9d136a8cadb
tita <inititaa23@gmail.com>
2025-10-01 15:05:17 +0700

    Menambahkan file MahasiswaView.java di folder view

---
+++ view/MahasiswaView.java
@@ -0,0 +1,13 @@
+package view;
+
+import model.Mahasiswa;
+
+public class MahasiswaView {
+
+    public static void main(String[] args) {
+        
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.ipk = 3.7f;
+    }
+
+}
